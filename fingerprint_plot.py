1a3b4b330b240fe1878309ad993ea457ccb38c04
John Roberts <jfr10@leicester.ac.uk>
2018-03-28 09:53:28 +0100

    Strating to work on converting datetime strings to datetime objects

--- fingerprint_plot.py
+++ fingerprint_plot.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+from datetime import datetime
 
 
 def fingerprint_plot(solar_timeseries):
     headings = solar_timeseries[0]
-    solar_timeseries.delete(0)
+    solar_timeseries = solar_timeseries[1:]
+    # convert to datetime object
+    time_format = "%Y-%m-%d %H:%M:%S"
+    dateview = solar_timeseries[:, 1]
+    for timestamp in np.nditer(dateview)
+
 
 
 def load_solar_data(csv_filepath):
