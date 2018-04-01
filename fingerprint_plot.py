4e9d9fe91402cd9fa2da9fb698be41c2ca645cdb
John <john@kaijugames.com>
2018-04-01 22:41:33 +0100

    Plotting is working now

--- fingerprint_plot.py
+++ fingerprint_plot.py
@@ -7,15 +7,27 @@ from datetime import datetime
 def fingerprint_plot(solar_timeseries):
     headings = solar_timeseries[0]
     solar_timeseries = solar_timeseries[1:]
+
     date_view = solar_timeseries[:, 1]
-    data_view = solar_timeseries[:, 2]  # Watch out for date_ and data_
     dates = [np.datetime64(dt) for dt in date_view]
     time_res = dates[1] - dates[0]
     y_axis = np.arange(np.timedelta64('00', 'h'), np.timedelta64('24', 'h'), time_res)
     x_axis = np.arange(dates[0], dates[-1], np.timedelta64('1', 'D'))
-    surf = data_view.reshape([x_axis.size, y_axis.size])
-    iris_plot = plt.contourf(x_axis, y_axis, surf)
+
+    data_view = solar_timeseries[:, 2]
+    for dp in np.nditer(data_view, op_flags=['readwrite']):
+        dp[...] = filter_na(dp)
+    data = data_view.astype(np.float)
+    surf = data.reshape([x_axis.size, y_axis.size])
+    iris_plot = plt.contourf(x_axis, y_axis, surf.T)
     plt.show()
+
+
+def filter_na(in_string):
+    if in_string == 'NA':
+        return '0'
+    else:
+        return in_string
 
 
 def load_solar_data(csv_filepath):
